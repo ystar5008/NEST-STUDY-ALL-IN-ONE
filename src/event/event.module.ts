1d50fc5b733ceae586f3016c385b1293ed538dad
index.ys <ystar5008@naver.com>
2024-10-06 15:58:57 +0900

    이그노어

--- src/event/event.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { EventService } from './event.service';
-import { EventGateway } from './event.gateway';
-
-@Module({
-  providers: [EventGateway, EventService],
-})
-export class EventModule {}
